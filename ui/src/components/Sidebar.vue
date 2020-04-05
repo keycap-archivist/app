0f3b9dfe31e3db21bdea5a8ad4bcf13a2f0a74c2
Vincent LE GOFF <vince.legoff@gmail.com>
2020-04-05 19:00:47 +0200

    Add about page

--- ui/src/components/Sidebar.vue
+++ ui/src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full md:w-1/12 bg-gray-900 md:bg-gray-900 px-2 text-center fixed bottom-0 md:pt-0 md:top-0 md:left-0 h-16 md:h-screen md:border-r-4 md:border-gray-600"
+    class="w-full md:w-2/12 lg:w-2/12 bg-gray-900 md:bg-gray-900 px-2 text-center fixed bottom-0 md:pt-0 md:top-0 md:left-0 h-16 md:h-screen md:border-r-4 md:border-gray-600"
   >
     <div class="md:relative pt-1 mx-auto lg:float-right lg:px-6">
       <ul class="list-reset flex flex-row md:flex-col text-center md:text-left">
@@ -26,15 +26,17 @@
           </router-link>
         </li>
         <li class="mr-3 flex-1">
-          <a
-            href="/api"
-            class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-yellow-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-yellow-500"
+          <router-link
+            tag="a"
+            to="/about"
+            :class="isActive('about', 'link')"
+            class="block py-1 md:py-3 pl-1 align-middle"
           >
             <i class="fas fa-link pr-0 md:pr-3"></i>
-            <span class="pb-1 md:pb-0 text-lg md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
-              >API Doc</span
+            <span class="pb-1 md:pb-0 text-lg md:text-base block md:inline-block" :class="isActive('about', 'span')"
+              >About</span
             >
-          </a>
+          </router-link>
         </li>
       </ul>
     </div>
--- ui/src/router.js
+++ ui/src/router.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import Router from "vue-router";
 import Catalog from "./views/Catalog.vue";
 import Wishlist from "./views/Wishlist.vue";
+import About from "./views/About.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     { path: "/wishlist", component: Wishlist, name: "wishlist" },
+    { path: "/about", component: About, name: "about" },
     { path: "*", component: Catalog, name: "catalog" }
   ]
 });
