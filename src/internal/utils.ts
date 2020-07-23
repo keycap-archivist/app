c80327506b6a8f997efebf1a388a5545676d28b9
Vincent LE GOFF <vince.legoff@gmail.com>
2020-07-23 20:21:41 +0200

    feat: add informations to submit caps

--- src/internal/utils.ts
+++ src/internal/utils.ts
@@ -164,6 +164,8 @@ export async function discordSubmissionUpdate(submission): Promise<void> {
 export async function discordSubmitCapName(colorway: ColorwayDetailed, name: string): Promise<void> {
   const output: string[] = [];
   output.push('**Colorway Name Correction**');
+  output.push(`Maker: ${colorway.maker}`);
+  output.push(`Sculpt: ${colorway.sculpt}`);
   if (colorway.name.length) {
     output.push(`Correction : ${colorway.name} => ${name}`);
   } else {
